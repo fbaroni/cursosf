ebaabef0c86f65c8313d524a6ca491e7530c11d4
Federico Baroni <fbaroni@santafe.gov.ar>
2017-11-06 14:01:09 +0000

    entidad Post

---
+++ src/AppBundle/Entity/Post.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="post")
+ */
+class Post
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $titulo;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $contenido;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $fecha;
+}
